b7d7c228711749b16b1ad1b05b4fffa5d8fe7529
Greee <no_signal@i.ua>
2024-05-24 08:45:20 +0300

    add_ioevennum

---
+++ ioeven.c
@@ -0,0 +1,39 @@
+// v1 от 2 до вводимого четные числа через запятую
+#include <stdio.h>
+int main (void) 
+{
+    // int input; // ввод
+    // int flag = 1; // флаг запятая
+    // int i; // счетчик
+    // printf("Enter number: "); 
+    // scanf("%d", &input);
+    // //printf("Input number = %d\n", input);
+    // printf("Even numbers : ");
+    // for(i = 0; i <= input; ++i) {
+    //     if(i % 2 == 0 && i != 0) {
+    //         if(flag == 0) {
+    //             printf(",");
+    //         }
+    //         printf("%d",i);
+    //         flag = 0;
+    //     }
+    // }
+    // puts("");
+
+    // v2 от 2 до вводимого четные числа через запятую
+    int i = 2;
+    int input = printf("Enter number : ");
+    scanf("%d", &input);
+    while(i <= input) {
+        if(i % 2 == 0) {
+            printf("%d", i);
+            if(i < input -1) {
+                printf(",");
+            }
+        }
+        i++;
+    }
+    puts("");
+    //printf("%d", input);
+}
+
